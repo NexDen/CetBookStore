66ec4355355f261192dda4588f2a3a8f817415fd
Hüseyin Şimşek <Huseyin.Simsek@obase.com>
2026-03-16 14:35:40 +0300

    HomePage

--- CetBookStore/Controllers/HomeController.cs
+++ CetBookStore/Controllers/HomeController.cs
@@ -1,18 +1,74 @@
+using CetBookStore.Data;
 using CetBookStore.Models;
+using CetBookStore.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace CetBookStore.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+
+        public async Task<IActionResult> Index()
         {
 
-            Book  book= new Book();
 
-            
-            return View();
+            HomePageViewModel homePageViewModel = new HomePageViewModel();
+
+
+           homePageViewModel.MostCommenteds =await context.Books.
+                OrderByDescending(b => b.Comments.Count)
+                .ThenByDescending(b => b.Title)
+                .Take(3)
+                .Select(b => new BookViewModel
+            {
+                Id = b.Id,
+                Title = b.Title,
+                Author = b.Author,
+                Price = b.Price,
+                OldPrice = b.PreviousPrice,
+                IsInSale = b.IsInSale
+
+                }).ToListAsync();
+
+            homePageViewModel.NewArrivals = await context.Books
+                .OrderByDescending(b => b.CreatedDate)
+                .ThenBy(b => b.Title)
+                .Take(3)
+                .Select(b => new BookViewModel
+                {
+                    Id = b.Id,
+                    Title = b.Title,
+                    Author = b.Author,
+                    Price = b.Price,
+                    OldPrice = b.PreviousPrice,
+                    IsInSale = b.IsInSale
+                }).ToListAsync();
+
+
+            homePageViewModel.RandomBoooks = await context.Books
+                .OrderBy(b => Guid.NewGuid())
+                .Take(6)
+                .Select(b => new BookViewModel
+                {
+                    Id = b.Id,
+                    Title = b.Title,
+                    Author = b.Author,
+                    Price = b.Price,
+                    OldPrice = b.PreviousPrice,
+                    IsInSale = b.IsInSale
+                }).ToListAsync();
+
+            return View(homePageViewModel);
         }
 
         public IActionResult Privacy()
---
+++ CetBookStore/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace CetBookStore.ViewModels
+{
+    public class BookViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+        public decimal OldPrice { get; set; }
+        public bool IsInSale { get; set; }
+   
+
+
+    }
+    public class HomePageViewModel
+    {
+
+
+        public List<BookViewModel> MostCommenteds { get; set; }=new List<BookViewModel>();
+        public List<BookViewModel> NewArrivals { get; set; }= new List<BookViewModel>();
+        public List<BookViewModel> RandomBoooks { get; set; } = new List<BookViewModel>();
+    }
+}
